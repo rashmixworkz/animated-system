9e171a1167af2255f4de2226851bf77944778c98
Rashmi Desai <rashmi17.xworkz@gmail.com>
2022-09-14 23:04:56 +0530

    rashmi

---
+++ Name.java
@@ -0,0 +1,62 @@
+class Name{
+	
+	static String getNameByMobileNumber(long number)
+{
+   System.out.println("entered getNameByMobileNumber");
+    if(number==8296122218L)
+       {
+       System.out.println("Rashmi mobile number is 8296122218");	
+       return "RASHMI";
+       }	
+
+    if(number==9482753620L)
+      {
+      System.out.println("Roopa mobile number is 9482753620");	
+      return "ROOPA";
+      }	
+
+    if(number==9880503325L)
+      {
+      System.out.println("Meghana  mobile number is 9880503325");	
+     return "MEGHANA";
+      }	
+
+    if(number==7829056966L)
+      {
+      System.out.println("Supriya mobile number is 7829056966");	
+      return "SUPRIYA";
+      }	
+
+    if(number==8151034234L)
+      {
+      System.out.println("Vaishnavi mobile number is8151034234");	
+      return "VAISHNAVI";
+      }	
+    if(number==9448958488L)
+     {
+     System.out.println("Nandini mobile number is 9448958488 ");	
+     return "NANDINI";
+     }	
+    if(number==8884182497L)
+     {
+     System.out.println("Salma mobile number is 8884182497");	
+     return "SALMA";
+     }	
+    if(number==6364156778L)
+     {
+    System.out.println("Gayithri mobile number is 6364156778");	
+    return "GAYITHRI";
+     }	
+    if(number==7618746859L)
+    {
+    System.out.println("Shweta mobile number is 7618746859");	
+    return "SHWETA";
+    }
+	if(number==8546805524L)
+    {
+    System.out.println("chinnu mobile number is 8546805524L");	
+    return "CHINNU";
+    }
+	return null;
+	}
+}
---
+++ NameRunner.java
@@ -0,0 +1,42 @@
+class NameRunner{
+	
+	public static void main(String[] name){
+		
+		String ref0=Name.getNameByMobileNumber(8296122218L);
+		System.out.println(ref0);
+		
+		
+		String ref1=Name.getNameByMobileNumber(9482753620L);
+		System.out.println(ref1);
+		
+		String ref2=Name.getNameByMobileNumber(9880503325L);
+		System.out.println(ref2);
+		
+		String ref3=Name.getNameByMobileNumber(7829056966L);
+		System.out.println(ref3);
+		
+		String ref4=Name.getNameByMobileNumber(8151034234L);
+		System.out.println(ref4);
+		
+		String ref5=Name.getNameByMobileNumber(9448958488L);
+		System.out.println(ref5);
+		
+		String ref6=Name.getNameByMobileNumber(8884182497L);
+		System.out.println(ref6);
+		
+		String ref7=Name.getNameByMobileNumber(8884182497L);
+		System.out.println(ref7);
+		
+		String ref8=Name.getNameByMobileNumber(7618746859L);
+		System.out.println(ref8);
+		
+		String ref9=Name.getNameByMobileNumber(8546805524L);
+		System.out.println(ref9);
+		
+		
+		
+		
+		
+		
+	}
+}
