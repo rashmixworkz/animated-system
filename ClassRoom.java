91893d49fab70ea592aa90d10ae59c7dd3623f3e
Rashmi Desai <rashmi17.xworkz@gmail.com>
2022-09-14 10:45:02 +0530

    hello

---
+++ ClassRoom.java
@@ -0,0 +1,24 @@
+class ClassRoom{
+	public static void main(String[] args){
+		short noOfStudents = 300;
+		short desks = 150;
+		short chairs = 600;
+		short noOfBags = 300;
+		short noOfBooks = 1000;
+		System.out.println("NoOfStudents:"+ noOfStudents);
+		System.out.println("Desks:"+ desks);
+		System.out.println("Chairs:"+ chairs);
+		System.out.println("NoOfBags:"+ noOfBags);
+		System.out.println("NoOfBooks:"+ noOfBooks);
+		noOfStudents = 1000;
+	    desks = 270;
+		chairs = 900;
+	    noOfBags = 1200;
+	    noOfBooks = 1500;
+		System.out.println("NoOfStudents:"+ noOfStudents);
+		System.out.println("Desks:"+ desks);
+		System.out.println("Chairs:"+ chairs);
+		System.out.println("NoOfBags:"+ noOfBags);
+		System.out.println("NoOfBooks:"+ noOfBooks);	
+	}
+}
