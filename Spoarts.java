31c6089242c68a751377819fa0f6cb12bb33fb51
rashmixworkz <113198016+rashmixworkz@users.noreply.github.com>
2022-09-20 08:24:08 +0530

    Add files via upload

---
+++ Spoarts.java
@@ -0,0 +1,60 @@
+class Spoarts
+{
+	static void cricketTeamMembers(String[] names)
+	{
+		System.out.println("starting");
+		System.out.println("Total no of team members names:"+names.length);
+		for(int index=0; index<names.length; index++)
+		{
+			String ref=names[index];
+			System.out.println(ref);
+			
+		}
+	}
+	static void KabbadiTeamMembersJerseyNos(int[] nos)
+	{
+		System.out.println("starting");
+		System.out.println("team members jersey number:"+nos.length);
+		for(int ram=0; ram<nos.length; ram++)
+		{
+			int ref=nos[ram];
+			System.out.println(ref);
+			
+		}
+	}
+	static void FootBallTeamMembersSalary(double[] salaries)
+	{
+		System.out.println("starting");
+		System.out.println("Football team members salaries in crore:"+salaries.length);
+		for(int num=0; num<salaries.length; num++)
+		{
+			double ref=salaries[num];
+			System.out.println(ref);
+			
+		}
+	}
+	static void KokoTeamMembersCountryCode(long[] names)
+	{
+		System.out.println("starting");
+		System.out.println("Total koko team members county code:"+names.length);
+		for(int sum=0; sum<names.length; sum++)
+		{
+			long ref=names[sum];
+			System.out.println(ref);
+			
+		}
+	}
+	
+	static void ludoTeamMobileNumber(long[] numbers)
+	{
+		System.out.println("starting");
+		System.out.println("ludo team members mobile number:"+numbers.length);
+		for(int joy=0; joy<numbers.length; joy++)
+		{
+			long ref=numbers[joy];
+			System.out.println(ref);
+			
+		}
+	}
+	
+}
---
+++ SpoartsRunner.java
@@ -0,0 +1,26 @@
+class SpoartsRunner
+{
+	public static void main(String[] args)
+	{
+		String[] players={"Kohili","Doni","K L Rahul","Hardhik pandya"};
+		 Spoarts.cricketTeamMembers( players);
+	
+
+         int[] number={9,18,5,25,17,12,19,23,2,77};
+		 Spoarts.KabbadiTeamMembersJerseyNos(number);
+		 
+		 
+		 double[] salary={5.4,3.1,4.5,2.1,1.2,3.3,2.3,5.5,1.3,1.9};
+		 Spoarts.FootBallTeamMembersSalary(salary);
+		 
+		 long[] code={91,86,92,7,62,880,98,81,};
+		 Spoarts.KokoTeamMembersCountryCode(code);
+		 
+		 long[] digits={8296122218L,9482753620L,9632629363L};
+		 Spoarts. ludoTeamMobileNumber(digits);
+		 
+		
+	
+	
+	}
+}
