2a2354ad929e56e99731985fd9bdacfe390c2a27
rashmixworkz <113198016+rashmixworkz@users.noreply.github.com>
2022-09-22 08:00:45 +0530

    Add files via upload

---
+++ GroceryStoreRunner.java
@@ -0,0 +1,37 @@
+class  GroceryStoreRunner
+{
+	
+	public static void main(String[] args)
+	{
+		    GroceryStore.name="Gajanana stores";
+		    GroceryStore.area="N R Colony";
+		    GroceryStore.item="Maggiee";
+		    GroceryStore.noOfDoors=2;
+			GroceryStore.storeCost=12300D;
+			GroceryStore.noOfEmployes=19;
+			GroceryStore.ownerName="Ganesh";
+			GroceryStore.branches="RR Nagar Indira nagar";
+			GroceryStore.noOfFloores=3;
+			GroceryStore.address="Opposite metro station National college Basavangudi" ;
+			GroceryStore.review="Best";
+			GroceryStore.ratings=4.5D;
+			
+			System.out.println("name of the store:"+GroceryStore.getname());
+			System.out.println("place:"+GroceryStore.getarea());
+			System.out.println("item:"+GroceryStore.getitem());
+			System.out.println("No of doors in store:"+GroceryStore.getnoOfDoors());
+			System.out.println("store cost is:"+GroceryStore.getstoreCost());
+			System.out.println("number of employes:"+GroceryStore.getnoOfEmployes());
+			System.out.println("owner name:"+GroceryStore.getownerName());
+			System.out.println("branches:"+GroceryStore.getbranches());
+			System.out.println("number of floores:"+GroceryStore.getnoOfFloores());
+			System.out.println("store address:"+GroceryStore.getaddress());
+			System.out.println("store review:"+GroceryStore.getreview());
+			System.out.println("store ratings:"+GroceryStore.getratings());
+		
+		
+			
+			
+			
+	}
+}
