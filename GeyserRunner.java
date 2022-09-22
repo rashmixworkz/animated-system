34935eb2fa2d47bd146b60f406710bbf5207a93f
rashmixworkz <113198016+rashmixworkz@users.noreply.github.com>
2022-09-22 07:59:37 +0530

    Add files via upload

---
+++ GeyserRunner.java
@@ -0,0 +1,18 @@
+class GeyserRunner{
+	public static void main(String[] args){
+		String brand=Geyser.brand;
+		int capacity=Geyser.capacity;
+		String power=Geyser.powerSource;
+		int wattage=Geyser.wattage;
+		String colour=Geyser.colour;
+		String material=Geyser.material;
+		
+		System.out.println(brand);
+		System.out.println(capacity);
+		System.out.println(power);
+		System.out.println(wattage);
+		System.out.println(colour);
+		System.out.println(material);
+		
+	}
+}
---
+++ GroceryStore.java
@@ -0,0 +1,124 @@
+class GroceryStore{
+	
+	static String name;
+	static String area;
+	static String item;
+	static int noOfDoors;
+	static double storeCost;
+	static int noOfEmployes;
+	static String ownerName;
+	static String branches;
+	static int noOfFloores;
+	static String address;
+	static String review;
+	static double ratings;
+	
+	
+	
+	static void setname(String valve)
+	{
+		name=valve;
+	}
+	static void setarea(String valve)
+	{
+		area=valve;
+	}
+	static void setitem(String valve)
+	{
+		item=valve;
+	}
+	static void setnoOfDoors(int valve)
+	{
+		noOfDoors=valve;
+	}
+	static void setstoreCost(double valve)
+	{
+		storeCost=valve;
+	}
+	static void setnoOfEmployes(int valve)
+	{
+		noOfEmployes=valve;
+	}
+	static void setownerName(String valve)
+	{
+		ownerName=valve;
+	}
+	static void setbranches(String valve)
+	{
+		branches=valve;
+	}
+	static void setnoOfFloores(int valve)
+	{
+		noOfFloores=valve;
+	}
+	static void setaddress(String valve)
+	{
+		address=valve;
+	}
+	static void setreviwe(String valve)
+	{
+		review=valve;
+	}
+	static void setratings(double valve)
+	{
+		ratings=valve;
+	}
+	
+	static String getname()
+	{
+		return name;
+	}
+	
+	static String getarea()
+	{
+		return area;
+	}
+	
+	static String getitem()
+	{
+		return item;
+	}
+	
+	static int getnoOfDoors()
+	{
+		return noOfDoors;
+	}
+	
+	static double getstoreCost()
+	{
+		return storeCost;
+	}
+	static int getnoOfEmployes()
+	{
+		return noOfEmployes;
+	}
+	static String getownerName()
+	{
+		return ownerName;
+	}
+	static String getbranches()
+	{
+		return branches;
+	}
+	static int getnoOfFloores()
+	{
+		return noOfFloores;
+	}
+	
+	static String getaddress()
+	{
+		return address;
+	}
+	static String getreview()
+	{
+		return review;
+	}
+	static double getratings()
+	{
+		return ratings;
+	}
+	
+	
+	
+	
+}
