3e783f0bcd4bd2e46f39992c9260d3f48beb36a6
Rashmi <113198016+rashmixworkz@users.noreply.github.com>
2022-09-30 09:28:18 +0530

    test

---
+++ testRashmi.java
@@ -0,0 +1,112 @@
+1)
+
+java is platform independant language.Byte code can run in any platform so byte code is called as platform indepandent.
+compiler converts java program into byte code that byte code can run in any os.
+bytecode is intermideate code that understand only jvm and jvm run the bytecode in any os.
+
+2)
+
+primitive datatype                                            non-primitive datatype
+
+there are 8 primitives datatypes                             there are n number of datatypes
+
+primitive datatypes does not have states & behaviours         non-primitive datatypes have states & behaviours
+
+ex: int,byte                                                   ex: arrays,String
+
+
+3)
+
+
+class classname
+{
+	class members;
+}
+
+class is templet or blueprint of objects.
+class members are variables which is used to store the valve and declared within class.
+
+ex:
+
+class Student
+{
+	String name;
+	int RollNumber;
+	String section;
+}
+
+4)
+class Percentage
+{
+  static double totalMarks;
+  static double receivedMarks;
+  static double percentage;
+  
+ static double student(String[] marks)
+{
+    System.out.println("start");
+	totalMarks=(receivedMarks/totalMarks)*100;
+	return percentage;
+   System.out.println("end");
+} 
+}
+class PercentageRunner
+{
+
+public static void main(String[] args)
+{
+	System.out.println("main method start");
+	Percentage.student(590,625);
+	System.out.println("main method end");
+	
+}
+}
+6)
+
+public static void main(String[] args)
+{
+	
+}
+public is access specifier;static is non-access specifier;void is return type;main is name of the method;
+()paranthesis; string[] args is parameters;{} is body;
+
+main method excutes in stack memory.
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
